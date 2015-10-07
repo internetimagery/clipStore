f4758700823634258e7fe0df55c81305a87759be
Jason <jason.dixon.email@gmail.com>
2015-10-08 11:28:02 +1300

    more work on file system

--- ziptest.py
+++ ziptest.py
@@ -44,22 +44,26 @@ import os
 import re
 import shutil
 
-class Store(object):
+class FileSystem(object):
     def __init__(s, path):
         """
         Working with a savefile
         """
-        s.list = []
         s.path = path
+        s.tempdir = tempfile.mkdtemp()
         if os.path.isfile(path):
             try: # Load our savefile
                 store = zipfile.ZipFile(path, "r")
-                s.list = [p for p in s.store.namelist()]
+                try:
+                    store.extractall(s.tempdir)
+                except (IOError, WindowsError):
+                    raise IOError, "Failed to load the file %s" % path
             except zipfile.BadZipfile:
                 print "File corrupt."
+                name, ext = os.path.splitext(path)
+                shutil.move(path, "%s(corrupt)%s" % (name, ext))
         else: # Init our save file
             zipfile.ZipFile(path, "w").close()
-        s.tempdir = tempfile.mkdtemp()
     def _formpath(s, path):
         if path:
             if os.path.isabs(path):
@@ -70,8 +74,31 @@ class Store(object):
         path = s._formpath(path)
         if "w" in mode: os.makedirs(os.path.dirname(path))
         return open(path, mode, buffering)
-    def mkdir(s, path, *args, **kwargs):
-        return os.makedirs(s._formpath(path), *args, **kwargs)
+    def remove(s, path):
+        path = s._formpath(root)
+        if os.path.isdir(path): os.removedirs(path)
+        elif os.path.isfile(path):
+            os.remove(path)
+            try: os.removedirs(os.path.dirname(path))
+            except OSError: pass
+    def save(s): # USE THIS TO PERSIST CHANGES!
+        f = tempfile.NamedTemporaryFile()
+        z = zipfile.ZipFile(f, "w")
+
+#
+#   def add_files(self, files=[]):
+#     for filename in files:
+#       if self.noisy:
+#         print "Adding file %s" % filename
+#
+#       if isdir(filename):
+#         for (dirname,dirs,filenames) in walk(filename):
+#           self.add_files(map(lambda x: "%s%s%s" % (dirname,sep,x),filenames))
+#       else:
+#         self.file.write(filename)
+#
+
+
     def __del__(s):
         print "cleaning up tempfiles"
         shutil.rmtree(s.tempdir)
@@ -82,9 +109,10 @@ path = os.path.join(
         ),
     "myfile.char")
 
-d = Store(path)
+d = FileSystem(path)
 with d.open("hone/two/three.txt", "w") as f:
-    print f.name
+    f.write("hello there")
+d.save()
 # if os.path.isfile(path):
 #     z = zipfile.ZipFile(path, "r")
 #     print z.namelist()
