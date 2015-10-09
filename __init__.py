f2273d2fa39795e1ae288b2e81ceb9a01447cd95
Internetimagery <jason.dixon.email@gmail.com>
2015-10-10 03:04:14 +1300

    slowly adding stuff

--- __init__.py
+++ __init__.py
@@ -1,1 +1,20 @@
-# Empty
+# Pick a software and lets do this!
+
+import i18n.en as i18n # ENGLISH
+import main
+
+def Start():
+    """
+    Lets do this!
+    """
+    try: # Test for Maya
+        import view.maya as view
+        import model.maya as model
+        main.Main(i18n, view, model, "maya")
+    except ImportError:
+        raise RuntimeError, "Sorry, your software is not supported."
+
+
+
+if __name__ == '__main__':
+    Start()
---
+++ main.py
@@ -0,0 +1,9 @@
+# Lets run this thing!!
+
+class Main(object):
+    def __init__(s, i18n, view, model, software):
+        s.i18n = i18n
+        s.view = view
+        s.model = model
+        s.software = software
+        print "lets go"
