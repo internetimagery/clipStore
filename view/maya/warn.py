1c75d54058e654a2405e408c97aa33b0051d2be0
Internetimagery <jason.dixon.email@gmail.com>
2015-10-11 11:01:19 +1300

    adding error handler

--- view/maya/warn.py
+++ view/maya/warn.py
@@ -1,11 +1,26 @@
-# Visual warning popup on error
+# Visual popup on errors. Not an error handler!
+# Created 11/10/15 Jason Dixon
+# http://internetimagery.com
 
 import maya.cmds as cmds
+import traceback
+import sys
 
-class Warn(object):
+class Safe(object):
+    def _err(s, title, message):
+        cmds.confirmDialog(
+            t="Uh oh... %s" % title,
+            m=message
+        )
+    def run(s, *args, **kwargs):
+        with Safe:
+            if len(args) and callable(args[0]):
+                return args[0](*args[1:], **kwargs)
+            else:
+                raise RuntimeError, "Function not provided as first argument."
     def __enter__(s):
-        return None
-    def __exit__(s, type, err, trace):
+        pass
+    def __exit__(s, *err):
         if err:
-            cmds.confirmDialog(t="Uh oh...", m=str(err))
-Warn = Warn()
+            s._err(err[0].__name__, err[1].message)
+Safe = Safe()
