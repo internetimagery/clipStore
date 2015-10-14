c631a791f0f65a3d9c468be78743ce9cb49cbc19
Jason <jason.dixon.email@gmail.com>
2015-10-15 06:43:02 +1300

    strip namespaces on select

--- model/maya/selection.py
+++ model/maya/selection.py
@@ -10,9 +10,9 @@ class Selection(object):
         Get current selection
         Return { object : { attribute : True/False } }
         """
-        objects = cmds.ls(sl=True)
+        objects = cmds.ls(sl=True, type="transform")
         channels = cmds.channelBox('mainChannelBox', sma=True, q=True)
         channels = [cmds.attributeQuery(at, n=objects[-1], ln=True) for at in channels] if objects and channels else []
-        return dict( (a, dict((b, True if not channels or b in channels else False) for b in cmds.listAttr(a, k=True) if cmds.attributeQuery(b, n=a, ex=True))) for a in cmds.ls(sl=True, type="transform") )
+        return dict( (a.replace(cmds.ls(a, sns=True)[1], "").replace(":", ""), dict((b, True if not channels or b in channels else False) for b in cmds.listAttr(a, k=True) if cmds.attributeQuery(b, n=a, ex=True))) for a in objects )
 
 Selection = Selection()
