3dc219f675a1fcc0f494f2920ba8a0bef5d940e5
Jason <jason.dixon.email@gmail.com>
2015-10-15 13:44:20 +1300

    put it back to using namespaces

--- model/maya/selection.py
+++ model/maya/selection.py
@@ -13,6 +13,7 @@ class Selection(object):
         objects = cmds.ls(sl=True, type="transform")
         channels = cmds.channelBox('mainChannelBox', sma=True, q=True)
         channels = [cmds.attributeQuery(at, n=objects[-1], ln=True) for at in channels] if objects and channels else []
-        return dict( (a.replace(cmds.ls(a, sns=True)[1], "").replace(":", ""), dict((b, True if not channels or b in channels else False) for b in cmds.listAttr(a, k=True) if cmds.attributeQuery(b, n=a, ex=True))) for a in objects )
+        return dict( (a, dict((b, True if not channels or b in channels else False) for b in cmds.listAttr(a, k=True) if cmds.attributeQuery(b, n=a, ex=True))) for a in objects )
+        # return dict( (a.replace(cmds.ls(a, sns=True)[1], "").replace(":", ""), dict((b, True if not channels or b in channels else False) for b in cmds.listAttr(a, k=True) if cmds.attributeQuery(b, n=a, ex=True))) for a in objects )
 
 Selection = Selection()
