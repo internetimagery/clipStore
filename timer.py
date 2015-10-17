9c0d244c5b2301577ce40208085732b6017da5ea
Jason <jason.dixon.email@gmail.com>
2015-10-18 01:34:34 +1300

    better use of import

--- timer.py
+++ timer.py
@@ -2,12 +2,12 @@
 # Created 18/10/15 Jason Dixon
 # http://internetimagery.com
 
+from time import time
+
 class Timer(object):
     verbose = False
     def __init__(s, name):
-        from time import time
-        s.time = time
         s.name = name
-    def __enter__(s): s.start = s.time()
+    def __enter__(s): s.start = time()
     def __exit__(s, *err):
-        if s.verbose: print "%s...\t\tElapsed time: %sms." % (s.name, (s.time() - s.start) * 1000)
+        if s.verbose: print "%s...\t\tElapsed time: %sms." % (s.name, (time() - s.start) * 1000)
