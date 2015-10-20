70a85d62906bed959a8ab3e84e1952b91ce27a68
Jason <jason.dixon.email@gmail.com>
2015-10-21 05:27:32 +1300

    adding base gui class for c4d

---
+++ view/c4d/baseGUI.py
@@ -0,0 +1,36 @@
+# Base for gui windows
+
+import c4d
+import c4d.gui as gui
+
+class Window(gui.geDialog):
+    """
+    Generic window
+    """
+    def __init__(s):
+        s._widgets = {}
+        s._id_start = 4000 # Where do we want to start our ID's?
+        gui.geDialog.__init__(s)
+    def CreateLayout(s):
+        raise NotImplementedError, "Override \"CreateLayout\"."
+    def Command(s, id, msg):
+        if id in s._widgets and callable(s._widgets[id]):
+            s._widgets(id, msg)
+    def Bind(s, id, func):
+        if id in s._widgets:
+            raise RuntimeError, "ID: %s is already bound!" % id
+        else:
+            s._widgets[id] = func
+    def Unbind(s, id=None, func=None):
+        if id:
+            del s._widgets[id]
+        elif func:
+            for a, b in s._widgets.items():
+                if func == b:
+                    del s._widgets[a]
+    def ID(s):
+        id = s._id_start
+        while id in s._widgets:
+            id += 1
+        s._widgets[id] = None
+        return id
